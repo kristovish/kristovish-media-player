1f8e1948b781b8d544be9aeb9e75cfed51fbc824
kristovish <info.torresdesantander@gmai.com>
2023-01-12 03:12:49 -0300

    pending improvements of version 1.2 made. improvements still need to be made for version 1.3

--- scripts.js
+++ scripts.js
@@ -1,28 +1,41 @@
 const audio = document.getElementById("audio");
-const playPause = document.getElementById("play");
 const volumeControl = document.getElementById("volume-control");
 const volumeDisplay = document.getElementById("volume-display");
 const stationMenu = document.querySelector('.station-menu');
 const mediaInfo = document.getElementById("media-info");
 
 
+let iscurrentStation;
+
+function activateStation(id) {
+  const activeStation = stations.find(station => station.name === id);
+  audio.src = activeStation.url;
+  audio.play();
+  // etc. para mostrar el nombre de la estación activa en el UI
+}
+
+const playPause = document.getElementById("play");
+const playIcon = document.querySelector(".fa-broadcast-tower");
+const pauseIcon = document.querySelector(".fa-pause-btn");
+
 playPause.addEventListener("click", () => {
   if (audio.paused || audio.ended) {
-    playPause.querySelector(".pause-btn").classList.toggle("hide");
-    playPause.querySelector(".play-btn").classList.toggle("hide");
     audio.play();
+    playIcon.classList.add("hide");
+    pauseIcon.classList.remove("hide");
   } else {
     audio.pause();
-    playPause.querySelector(".pause-btn").classList.toggle("hide");
-    playPause.querySelector(".play-btn").classList.toggle("hide");
+    playIcon.classList.remove("hide");
+    pauseIcon.classList.add("hide");
   }
 });
+
 
 //10 de Enero
 
 
 
-const previous = document.getElementById("previous");
+const previous = document.getElementById("star");
 let mediaInfoVisible = false;
 
 previous.addEventListener("click", () => {
@@ -64,45 +77,117 @@ const stations = [
   {
     name: 'Cosmos Radio',
     url: 'http://cosmosfm.stream.laut.fm/cosmosfm?'
-  }
+  },
+  {
+    name: 'Prambos Radio',
+    url: 'https://22283.live.streamtheworld.com:443/PRAMBORS_FM.mp3?dist=onlineradiobox'
+  },
+  {
+    name: 'I-Radio',
+    url: 'https://n03.radiojar.com/4ywdgup3bnzuv?rj-ttl=5&rj-tok=AAABe120nigAlANV6qVbAOh-ZQ'
+  },
+  {
+    name: 'Denger Radio',
+    url: 'http://stream.denger.in:8888/dmi'
+  },
+  {
+    name: 'Skonto Radio',
+    url: 'https://stream.radioskontoplus.lv:8443/st128'
+  },
+  {
+    name: 'Kiss Radio',
+    url: 'https://radioindia.net/radio/kissfmindia/icecast.audio'
+  },
+  {
+    name: 'Cassete Radio',
+    url: 'https://zas1.ndx.co.za:8158/stream?1596196923074'
+  },
+  {
+    name: 'E! Radio',
+    url: 'https://207.148.74.192:7874/stream.mp3'
+  },
 ];
+const workingStations = [];
+
+stations.forEach(station => {
+  // Enviamos una solicitud GET a la URL del stream
+  fetch(station.url)
+    .then(response => {
+      // Si el estado es 200, la estación está funcionando
+      if (response.status === 200) {
+        workingStations.push(station);
+      }
+    })
+    .catch(error => {
+      console.error(`Error al obtener la estación ${station.name}: ${error}`)
+      //elimina elementos del reproductor con enlaces rotos
+      const element = document.getElementById(station.name);
+      element.parentNode.removeChild(element);
+    })
+    .finally(() => {
+      // Asignamos el array de estaciones funcionando a la variable stations
+      stations = workingStations;
+    });
+});
+
+
 
 // Obtenemos el botón del menú y la lista de estaciones
 const menuBtn = document.getElementById('icon-menu');
 const menu = document.querySelector('.station-menu');
 
-menuBtn.addEventListener('click', function(){
+menuBtn.addEventListener('click', function () {
   menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 });
 
 
 
 // Agregamos un evento click al botón del menú para mostrar/ocultar el menú
-menuBtn.addEventListener('click', function() {
+menuBtn.addEventListener('click', function () {
   menu.classList.toggle('show-menu');
 });
 
 
 // obtenemos cada uno de los hipervinculos
-const stationLinks = document.querySelectorAll('.station-link');
-
-// Agregamos un evento click a cada hipervínculo de estación para activar la estación correspondiente
-stationLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
-    activateStation(link.id);
+// Obtenemos todos los elementos del menú que tienen la clase "station-link"
+const stationLinks = document.querySelectorAll(".station-link");
+stationLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    activateStation(this.id);
   });
 });
-
-
-
-// La función activateStation sería la encargada de activar la estación correspondiente
 function activateStation(id) {
   const activeStation = stations.find(station => station.name === id);
   audio.src = activeStation.url;
   audio.play();
   // etc. para mostrar el nombre de la estación activa en el UI
+  playIcon.classList.add("hide");
+  pauseIcon.classList.remove("hide");
 }
 
+for (let i = 0; i < stationLinks.length; i++) {
+  stationLinks[i].addEventListener("click", function () {
+    activateStation(this.id);
+  });
+}
+
+function activateStation(id) {
+  const activeStation = stations.find(station => station.name === id);
+  if (!activeStation.url) {
+    document.getElementById("station-name").innerHTML = "Emisora no disponible";
+    document.getElementById("station-name").style.color = "red";
+    return;
+  }
+  audio.src = activeStation.url;
+  audio.play();
+  document.getElementById("station-name").innerHTML = activeStation.name;
+  document.getElementById("station-name").style.color = "black";
+}
+
+audio.onerror = function () {
+  // Aquí puedes mostrar un mensaje de error al usuario indicando que la emisora no está disponible
+  document.querySelector(".player__title").innerHTML = "Not Available In Your Place";
+};
 
 const stationName = document.getElementById("station-name");
 
@@ -111,19 +196,28 @@ function activateStation(id) {
   const activeStation = stations.find(station => station.name === id);
   audio.src = activeStation.url;
   audio.play();
-  
+
   // actualizar el nombre de la estación en la UI
   stationName.textContent = activeStation.name;
 }
 
 
 // Evento para volver a mostrar la etiqueta "Kristovish Media Player" cuando se pausa la reproducción
-audio.addEventListener('pause', function() {
+audio.addEventListener('pause', function () {
   stationName.textContent = "Kristovish Media Player";
 });
 
-stationLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
+
+audio.addEventListener('play', () => {
+  document.querySelector('.player__title').innerHTML = 'Playing now';
+});
+audio.addEventListener('pause', () => {
+  document.querySelector('.player__title').innerHTML = 'Radio Player';
+});
+
+
+stationLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
     // Obtenemos el id del enlace clickeado
     const stationId = link.id;
     // Obtenemos el stream de la estacion 
@@ -139,34 +233,37 @@ stationLinks.forEach(function(link) {
 let currentStation = 0;
 
 const nextStation = () => {
-    currentStation = currentStation === stations.length  ? 0 : currentStation + 1;
-    audio.src = stations[currentStation].url;
-    audio.play();
-    stationName.innerHTML = stations[currentStation].name;
+  currentStation = currentStation === stations.length ? 0 : currentStation + 1;
+  audio.src = stations[currentStation].url;
+  audio.play();
+  stationName.innerHTML = stations[currentStation].name;
 }
 
 const prevStation = () => {
-    currentStation = currentStation === 0 ? stations.length - 1 : currentStation - 1;
-    audio.src = stations[currentStation].url;
-    audio.play();
-    stationName.innerHTML = stations[currentStation].name;
+  currentStation = currentStation === 0 ? stations.length - 1 : currentStation - 1;
+  audio.src = stations[currentStation].url;
+  audio.play();
+  stationName.innerHTML = stations[currentStation].name;
 }
 
 document.getElementById("forward").addEventListener("click", nextStation);
 document.getElementById("backward").addEventListener("click", prevStation);
 
+
+
 const forwardBtn = document.getElementById("forward");
 forwardBtn.addEventListener("click", () => {
-    // si la estación actual es la última del arreglo, selecciona la primera
-    if(currentStation === stations[stations.length-1]) {
-        currentStation = stations[0];
-    } else {
-        // si no, selecciona la siguiente estación
-        currentStation = stations[stations.indexOf(currentStation)+1];
-    }
-    // activa la estación seleccionada
-    activateStation(currentStation);
+  currentStationIndex = (currentStationIndex + 1) % stations.length;
+  activateStation();
 });
+
+const backwardBtn = document.getElementById("backward");
+backwardBtn.addEventListener("click", () => {
+  currentStationIndex = (currentStationIndex - 1 + stations.length) % stations.length;
+  activateStation();
+});
+
+
 
 
 audio.volume = 0.5;
@@ -182,7 +279,7 @@ function setVolume(value) {
 const volumeRange = document.getElementById("volume-control");
 const volumePercentage = document.getElementById("volume-display");
 
-volumeRange.addEventListener("input", function() {
+volumeRange.addEventListener("input", function () {
   const volumeValue = this.value;
   volumePercentage.innerHTML = `${volumeValue}%`;
 });
